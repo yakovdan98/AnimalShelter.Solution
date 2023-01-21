dc0b84a672d144188cac08947880f521621b78cc
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-01-20 17:08:49 -0800

    refactored files

---
+++ ShelterApi/Controllers/v1/AnimalsController.cs
@@ -0,0 +1,95 @@
+using Microsoft.EntityFrameworkCore;
+using ShelterApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShelterApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly ShelterApiContext _db;
+
+    public AnimalsController(ShelterApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    {
+      return await _db.Animals.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+
+      if (animal == null)
+      {
+        return NotFound();
+      }
+      return animal;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      Animal originalAnimal = await _db.Animals.FindAsync(id);
+
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Animals.Update(animal);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
+}
---
+++ ShelterApi/Models/Animal.cs
@@ -0,0 +1,10 @@
+namespace ShelterApi.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; } 
+    public int Age { get; set; }
+  }
+}
