13059ffd922a00ca90f4ab6954641300e1d5e1f4
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-01-20 17:03:02 -0800

    added cersioning

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using ShelterApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,16 @@ builder.Services.AddDbContext<ShelterApiContext>(
                     )
                   )
                 );
+
+builder.Services.AddApiVersioning(opt =>
+                    {
+                      opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                      opt.AssumeDefaultVersionWhenUnspecified = true;
+                      opt.ReportApiVersions = true;
+                      opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                      new HeaderApiVersionReader("x-api-version"),
+                                                                                      new MediaTypeApiVersionReader("x-api-version"));
+                    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,10 +32,10 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
-else 
+else
 {
   app.UseHttpsRedirection();
 }
